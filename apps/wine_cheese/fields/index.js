c5ad20aa3baa2657705bc88bd0471d6be4c3ad2d
Alex Swann <alex_swann@hotmail.co.uk>
2016-11-29 14:09:46 +0000

    setup steps and fields

--- apps/wine_cheese/fields/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var _ = require('underscore');
-
-module.exports = _.extend(
-);
---
+++ apps/wine_cheese/steps.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  '/': {
+    controller: require('../common/controllers/start'),
+    next: '/choices'
+  },
+  '/choices': {
+    controller: require('./controllers/choices'),
+    template: 'choices',
+    fields: [
+      'yes-no-radio-toggler',
+      'example-toggled-text'
+    ],
+    next: '/confirmation'
+  },
+  '/confirmation': {
+    template: 'confirmation.html',
+    backLink: false,
+    clearSession: true
+  }
+}
+;
